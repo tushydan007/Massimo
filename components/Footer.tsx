541d64a48503d80dd283207689ac993c72536872
Cornelius Ezeh <cornzeh@gmail.com>
2024-11-28 12:32:28 +0100

    Changed the color of the Footer and the Navbar to white

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
-      <Link href="/" className="font-bold text-xl">MASSIMO</Link>
+    <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between bg-white">
+      <Link href="/" className="font-bold text-xl">
+        MASSIMO
+      </Link>
       <p>© ALL RIGHTS RESERVED.</p>
     </div>
   );
